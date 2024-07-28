7b55fb46b49170dafb6b95b74d1c148fffd97157
TimWitzdam <tim2013w@gmail.com>
2024-07-28 16:27:19 +0200

    Added functionality to show zones not in folders

--- cloudflare_dings.js
+++ cloudflare_dings.js
@@ -10,34 +10,56 @@ let folders = [
     name: "Test folder 2",
     zones: ["cryptodisplay.net"],
   },
+  {
+    id: 3,
+    name: "Test folder 3",
+    zones: [],
+  },
 ];
 
-function getZones() {
+function getIDFromZone(zone) {
+  return zone.getAttribute("data-testid").split("zone-card-")[1];
+}
+
+function hideZones() {
   let zones = document.querySelectorAll('[data-testid^="zone-card-"]');
+  let zonesInFolders = folders.reduce((acc, folder) => {
+    return acc.concat(folder.zones);
+  }, []);
+
   for (zoneEl of zones) {
-    zoneEl.style.display = "none";
-  }
-  for (folder of folders.reverse()) {
-    createFolderElement(folder);
+    if (zonesInFolders.includes(getIDFromZone(zoneEl)))
+      zoneEl.style.display = "none";
   }
 }
 
+function changeZoneAllZoneVisability(visible) {
+  let zones = document.querySelectorAll('[data-testid^="zone-card-"]');
+  for (zone of zones) {
+    zone.style.display = visible ? "flex" : "none";
+  }
+}
+
+function getZones() {
+  for (folder of folders.reverse()) {
+    createFolderElement(folder);
+  }
+  hideZones();
+}
+
 function openFolder(folderID) {
-  const folder = folders.filter((folder) => folder.id === folderID)[0];
+  const folder = folders.find((folder) => folder.id === folderID);
   let folderElements = document.querySelectorAll('[id^="folder-"]');
   let zoneCardsEl = document.querySelector('[data-testid="zone-cards"]');
   let zones = document.querySelectorAll('[data-testid^="zone-card-"]');
 
+  changeZoneAllZoneVisability(false);
   for (fEl of folderElements) {
     fEl.style.display = "none";
   }
 
   for (zoneEl of zones) {
-    if (
-      folder.zones.includes(
-        zoneEl.getAttribute("data-testid").split("zone-card-")[1]
-      )
-    ) {
+    if (folder.zones.includes(getIDFromZone(zoneEl))) {
       zoneEl.style.display = "flex";
     }
   }
@@ -65,9 +87,8 @@ function openFolder(folderID) {
       fEl.style.display = "flex";
     }
 
-    for (zoneEl of zones) {
-      zoneEl.style.display = "none";
-    }
+    changeZoneAllZoneVisability("true");
+    hideZones();
 
     backButton.remove();
   });
@@ -84,7 +105,7 @@ function createFolderElement(folder) {
     }
   }
   newFolderEl.innerHTML = `
-<div style="display:flex;flex-direction: column;justify-content: space-between;">
+<div style="display:flex;flex-direction: column;justify-content: space-between;gap: 0.5rem;">
     <div style="display:flex;align-items: center;gap: 0.5rem">
         <svg width="20" height="16" viewBox="0 0 27 23" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M3.375 23H23.625C25.4865 23 27 21.5266 27 19.7143V6.57143C27 4.75915 25.4865 3.28571 23.625 3.28571H15.1875C14.6549 3.28571 14.1539 3.04442 13.8375 2.62857L12.825 1.31429C12.1869 0.487723 11.185 0 10.125 0H3.375C1.51348 0 0 1.47344 0 3.28571V19.7143C0 21.5266 1.51348 23 3.375 23Z" fill="#313131"/>
@@ -92,7 +113,7 @@ function createFolderElement(folder) {
         <span>${folder.name}</span>
     </div>
     <div style="background-color: rgb(242, 242, 242);color: black;border-radius: 50px;width: fit-content;font-size: 12px;padding-bottom: 4px; padding-top: 4px;padding-left: 8px;padding-right: 8px;">
-        ${folder.zones.length} Zones
+        ${folder.zones.length} Zone${folder.zones.length === 1 ? "" : "s"}
     </div>
 </div>
   `;
